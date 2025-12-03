63b1eb6c1b8050f66417cc0c2c4931a262658af8
Kalyani-bhandari <bhandarikalyani2057@gmail.com>
2025-12-03 15:04:14 -0700

    Update fastqc.sh

--- fastqc.sh
+++ fastqc.sh
@@ -95,17 +95,17 @@ fastp \
 cd /home/g89x126/sc_wgs
 
 # samtools index
-samtools faidx reference.fasta
+samtools faidx reference.fa
 
 # bwa index
-bwa index reference.fasta
+bwa index reference.fa
 
 #align_trimmed_file_with_reference(align_one.sh)
   #!/bin/bash
 
 conda activate wgs_env
 
-REF="/home/g89x126/sc_wgs/reference.fasta"
+REF="/home/g89x126/sc_wgs/reference.fa"
 TRIMDIR="/home/g89x126/sc_wgs/fastp_out"
 OUTDIR="/home/g89x126/sc_wgs/bam"
 
@@ -170,6 +170,169 @@ bcftools filter \
 
 bcftools index vcf_filtered/5-Rest.filtered.vcf.gz
 
+#Extract_EMS_Mutation_only
+mkdir -p EMS_vcfs
+
+for VCF in vcf_filtered/*.filtered.vcf.gz; do
+    BASE=$(basename "$VCF" .filtered.vcf.gz)
+    
+    bcftools view \
+      -i 'TYPE="snp" && ((REF="G" && ALT="A") || (REF="C" && ALT="T"))' \
+      "$VCF" \
+      -Oz -o EMS_vcfs/${BASE}.EMS.vcf.gz
+    
+    bcftools index EMS_vcfs/${BASE}.EMS.vcf.gz
+done
+#countit
+bcftools stats EMS_vcfs/4-Rest.EMS.vcf.gz | grep "number of records"
+
+
+#FILTER_ONLY_REST_OR_ONLY_SUSP
+
+bcftools isec \
+    -p compare4 \
+    EMS_vcfs/4-Rest.EMS.vcf.gz \
+    EMS_vcfs/4-Susp.EMS.vcf.gz
+
+
+bgzip compare4/0000.vcf
+bcftools index compare4/0000.vcf.gz
+
+bgzip compare4/0001.vcf
+bcftools index compare4/0001.vcf.gz
+
+bcftools isec \
+    -p compare5 \
+    EMS_vcfs/5-Rest.EMS.vcf.gz \
+    EMS_vcfs/5-Susp.EMS.vcf.gz
+
+bgzip compare5/0000.vcf
+bcftools index compare5/0000.vcf.gz
+
+bgzip compare5/0001.vcf
+bcftools index compare5/0001.vcf.gz
+
+#count
+bcftools view -H compare4/4-Rest_only.vcf.gz | wc -l    # unique to 4-Rest
+bcftools view -H compare4/4-Susp_only.vcf.gz | wc -l    # unique to 4-Susp
+bcftools view -H compare4/0002.vcf.gz| wc -l    # shared
+
+bcftools view -H compare4/4-Rest_only.vcf.gz | head
+bcftools view -H compare4/4-Susp_only.vcf.gz | head
+
+
+bcftools view -H compare4/4-Rest_only.vcf.gz | wc -l
+bcftools view -H compare4/4-Susp_only.vcf.gz | wc -l
+bcftools view -H compare4/0002.vcf.gz | wc -l
+
+
+#common snps in both resistance lines 4 and that is not in susp
+
+# 1. Shared resistant SNPs
+bcftools isec -n=2 EMS_vcfs/4-Rest.EMS.vcf.gz EMS_vcfs/5-Rest.EMS.vcf.gz -p Rest_shared
+bgzip Rest_shared/0000.vcf
+bcftools index Rest_shared/0000.vcf.gz
+
+# 2. Shared susceptible SNPs
+bcftools isec -n=2 EMS_vcfs/4-Susp.EMS.vcf.gz EMS_vcfs/5-Susp.EMS.vcf.gz -p Susp_shared
+
+bgzip Susp_shared/0000.vcf
+bcftools index Susp_shared/0000.vcf.gz
+
+# 3. Resistance-specific SNPs
+bcftools isec Rest_shared/0000.vcf.gz Susp_shared/0000.vcf.gz -p Resistance_unique
+
+bgzip Resistance_unique/0000.vcf
+bcftools index Resistance_unique/0000.vcf.gz
+
+# 4. Count final candidates
+bcftools view -H Resistance_unique/0000.vcf.gz | wc -l
+bcftools view -H Resistance_unique/Resistance_unique.vcf.gz | head
+
+#5. Susceptible_unique_SNPs
+bcftools isec \
+    Susp_shared/0000.vcf.gz \
+    Rest_shared/0000.vcf.gz \
+    -p Susceptible_unique
+
+bgzip Susceptible_unique/0000.vcf
+bcftools index Susceptible_unique/0000.vcf.gz
+
+
+# 6. Count final candidates
+bcftools view -H Susceptible_unique/0000.vcf.gz | wc -l
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#annotation
+#snepff_database
+mkdir -p snpeff/data/solanum_chacoense_m6
+
+cp reference.fa snpeff/data/solanum_chacoense_m6/sequences.fa
+cp m6.hc.gene_models.all_chrs_corrected.gff3 snpeff/data/solanum_chacoense_m6/genes.gff
+
+find snpeff -name snpEff.config
+nano snpeff/snpEff.config
+solanum_chacoense_m6.genome : solanum_chacoense_m6
+
+cd snpeff
+java -Xmx8g -jar snpEff.jar build -gff3 -v solanum_chacoense_m6
+
+cd snpeff
+
+wget https://snpeff.blob.core.windows.net/versions/snpEff_latest_core.zip
+unzip snpEff_latest_core.zip
+
+mkdir -p $CONDA_PREFIX/share/snpeff/data/solanum_chacoense_m6
+
+conda activate wgs_env
+conda install -c bioconda snpeff
+
+bcftools csq \
+   --force \
+   -f reference.fa \
+   -g m6.hc.gene_models.all_chrs_corrected.gff3 \
+   EMS_vcfs/4-Rest.EMS.vcf.gz \
+   -Oz -o EMS_vcfs/4-Rest.EMS.annotated.vcf.gz
+
+   #another_file_for_annotation
+   bcftools csq --force \
+    -f reference.fa \
+    -g m6.hc.gene_models.chr_corrected.gff3 \
+    EMS_vcfs/4-Rest.EMS.vcf.gz \
+    -Oz -o EMS_vcfs/4-Rest.EMS.annotated.v2.vcf.gz
+
+   
+bcftools +split-vep EMS_vcfs/4-Rest.EMS.annotated.vcf.gz \
+    -d both \
+    -Oz -o EMS_vcfs/4-Rest.EMS.annotated.split.vcf.gz
+bcftools +split-vep -l -a BCSQ EMS_vcfs/4-Rest.EMS.annotated.vcf.gz
+
+bcftools +split-vep \
+    -a BCSQ \
+    -c Consequence,gene,transcript,amino_acid_change,dna_change \
+    -s worst \
+    -p BCSQ \
+    EMS_vcfs/4-Rest.EMS.annotated.vcf.gz \
+    -Oz -o EMS_vcfs/4-Rest.EMS.split.vcf.gz
+
+bcftools index -f EMS_vcfs/4-Rest.EMS.split.vcf.gz
+
+
+
+
 #Compare_Resistant_vs_Susceptible_VCFs
 
 #Resistant_shared_mutations:
@@ -229,10 +392,17 @@ bcftools view -i 'TYPE="indel"' compare/final/0000.vcf.gz | wc -l
 bcftools filter -i 'MQ>=40 && DP>=10 && TYPE="snp"' \
    compare/ems_filtered/0000.EMS.vcf.gz \
 | bcftools csq --force \
-   -f reference.fasta \
+   -f reference.fa \
    -g reference.gff3 \
    -Oz -o EMS.csq.vcf.gz
+grep -v "#" reference.gff3 | cut -f1 | head
+grep -v "#" m6.hc.gene_models.chr_corrected.gff3 | cut -f1 | head
 
+bcftools csq --force \
+   -f reference.fa \
+   -g m6.hc.gene_models.chr_corrected.gff3 \
+   -Oz -o EMS.annotated.vcf.gz \
+   compare/ems_filtered/0000.EMS.vcf.gz
 
 
 
