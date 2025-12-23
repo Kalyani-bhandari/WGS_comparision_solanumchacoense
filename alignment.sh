c718b3d36e6e14fc4d0d36fb708a1fb8568788c7
Kalyani Bhandari <bhandarikalyani2057@gmail.com>
2025-12-23 15:53:29 -0700

    Refactor alignment.sh
    Removed commented sections and updated comments for clarity.

--- alignment.sh
+++ alignment.sh
@@ -16,9 +16,8 @@ OUTDIR="/home/g89x126/sc_wgs/bam"
 
 mkdir -p $OUTDIR logs
 
-# ---------------------------------------------------------
-# 3. Create sample list automatically
-# ---------------------------------------------------------
+
+#Create_sample_list_automatically
 SAMPLES=( $(ls $RAW_DIR/*_R1_trimmed.fastq.gz | sed 's/_R1_trimmed.fastq.gz//' | xargs -n1 basename) )
 
 SAMPLE=${SAMPLES[$SLURM_ARRAY_TASK_ID]}
@@ -30,17 +29,14 @@ echo "Processing sample: $SAMPLE"
 echo "R1 = $R1"
 echo "R2 = $R2"
 
-# ---------------------------------------------------------
-# 4. Index reference (done only once)
-# ---------------------------------------------------------
+#Index_reference
+
 if [ ! -f "${REF}.bwt" ]; then
     echo "Indexing reference genome with BWA..."
     bwa index $REF
 fi
 
-# ---------------------------------------------------------
-# 5. Alignment → Sorting → BAM index
-# ---------------------------------------------------------
+#Alignment_Sorting_BAM index
 bwa mem -t 8 $REF $R1 $R2 \
     | samtools view -bS -@ 4 - \
     | samtools sort -@ 4 -o $OUTDIR/${SAMPLE}.sorted.bam -
