a8ce55f5725eac2b932b75f64a0b25414204d956
Kalyani Bhandari <bhandarikalyani2057@gmail.com>
2025-12-23 15:22:51 -0700

    Update paths.sh

--- config/paths.sh
+++ config/paths.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
-
-# ================================
 # Reference genome and annotation
-# ================================
 
 # Update these paths on the HPC system (Tempest)
 REF="/home/g89x126/sc_wgs/reference.fa"
 GFF="/home/g89x126/sc_wgs/m6.hc.gene_models.all_chrs_corrected.gff3"
 
-# ================================
-# Output directories (relative to repo root)
-# ================================
 
+# Output directories (relative to repo root)
 FASTP_DIR="results/fastp"
 FASTQC_RAW_DIR="results/fastqc_raw"
 FASTQC_TRIM_DIR="results/fastqc_trimmed"
 BAM_DIR="results/bam"
 VCF_DIR="results/vcf"
 
-# ================================
 # SnpEff paths
-# ================================
 SNPEFF_DIR="/home/g89x126/sc_wgs/snpeff"
 SNPEFF_DATA="$SNPEFF_DIR/data/solanum_chacoense_m6"
 
